3ae456283bce29b6d1c816f329b4361439e38250
GourabGorai <gourabg30march@gmail.com>
2024-12-15 19:48:33 +0500

    Delete src directory

--- src/components/layout/Background/Grid.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Grid() {
-  return (
-    <div className="absolute inset-0">
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:64px_64px] [transform-style:preserve-3d] [transform:rotateX(60deg)] dark:bg-[linear-gradient(to_right,#ffffff0a_1px,transparent_1px),linear-gradient(to_bottom,#ffffff0a_1px,transparent_1px)]" />
-    </div>
-  );
-}
